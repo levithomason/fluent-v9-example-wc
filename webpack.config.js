638b18463e73438f1faf23b7e79de3e175189008
Chris Holt <chhol@microsoft.com>
2022-02-14 20:04:50 -0800

    quick and dirty react application to illustrate interop w/ fluent theme provider

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+
+const appDir = path.resolve("./app");
+const outDir = path.resolve("./www");
+
+module.exports = () => {
+    return {
+        devServer: {
+            compress: false,
+            open: true,
+            port: 9005,
+            static: {
+                directory: path.join(__dirname, 'www'),
+            },
+        },
+        mode: "development",
+        devtool: "inline-source-map",
+        entry: appDir + "/index.tsx",
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?/,
+                    use: [{
+                        loader: 'ts-loader',
+                        options: {
+                            configFile: "tsconfig.app.json"
+                        }
+                    }],
+                    exclude: /node_modules/,
+                }
+            ]
+        },
+        output: {
+            path: outDir,
+            publicPath: "/",
+            filename: "[name].js",
+        },
+        resolve: {
+            extensions: [".js", ".jsx", ".ts", ".tsx"],
+        },
+    };
+};
