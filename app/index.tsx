9844d1f07c3367901f17a1d22da2701b7b82d375
Wendy <wendy@Wendys-MacBook-Pro-Microsoft.local>
2022-02-25 14:36:16 -0800

    enable deploy for fluent button only

--- app/index.tsx
+++ app/index.tsx
@@ -4,19 +4,18 @@ import {
     FluentProvider,
     webLightTheme } from '@fluentui/react-components';
 import { provideReactWrapper } from '@microsoft/fast-react-wrapper';
-import { fluentButton, provideFluentDesignSystem } from "../src/index";
+import { FluentButton } from "../src/index";
 
 const layoutStyles = { display: "grid", gap: "12px", padding: "12px" }
 const { wrap } = provideReactWrapper(
-    React, 
-    provideFluentDesignSystem()
+    React
 );
 
-export const FluentButton = wrap(fluentButton());
+export const FluentButtonTest = wrap(FluentButton);
 ReactDOM.render(
     <FluentProvider style={{ display: "grid", gap: "12px"}} theme={webLightTheme}>
         <div style={layoutStyles}>
-            <FluentButton appearance="primary">Button</FluentButton>
+            <FluentButtonTest appearance="primary">Test</FluentButtonTest>
         </div>
     </FluentProvider>,
     document.getElementById('root'),
