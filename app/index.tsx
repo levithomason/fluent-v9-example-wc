c5eca7dbefb331c4062e0ed999705d75d4e4fde8
Wendy <wendy@Wendys-MacBook-Pro-Microsoft.local>
2022-02-28 11:45:05 -0800

    add design system back

--- app/index.tsx
+++ app/index.tsx
@@ -4,11 +4,11 @@ import {
     FluentProvider,
     webLightTheme } from '@fluentui/react-components';
 import { provideReactWrapper } from '@microsoft/fast-react-wrapper';
-import { fluentButton } from "../src/index";
+import { fluentButton, provideFluentDesignSystem } from "../src/index";
 
 const layoutStyles = { display: "grid", gap: "12px", padding: "12px" }
 const { wrap } = provideReactWrapper(
-    React
+    React, provideFluentDesignSystem()
 );
 
 export const FluentButtonTest = wrap(fluentButton());
