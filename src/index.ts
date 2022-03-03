a37273edb66045647c590db50878d77ac6bf0ae2
Wendy <wendy@Wendys-MacBook-Pro-Microsoft.local>
2022-03-02 16:27:07 -0800

    working

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
  * Export all custom element definitions
  */
 export * from "./button/index";
+<<<<<<< HEAD
 export * from "./card/index";
 export * from "./card-footer/index";
 export * from "./card-header/index";
@@ -10,3 +11,7 @@ export * from "./divider/index";
 export * from "./fluent-design-system";
 export * from "./link/index";
 export * from "./presence-badge/index";
+=======
+export * from "./link/index";
+export * from "./fluent-design-system";
+>>>>>>> 4725917 (working)
