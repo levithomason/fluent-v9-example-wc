e163f87ec21a9d393182d1afc446e96ffbd93b57
Wendy <wendy@Wendys-MacBook-Pro-Microsoft.local>
2022-03-04 09:26:32 -0800

    add missing dep

--- src/button/index.ts
+++ src/button/index.ts
@@ -1,3 +1,4 @@
+import React from 'react'
 import { customElement } from "@microsoft/fast-element";
 import { Button } from "./button";
 import { buttonTemplate as template } from "./button.template";
